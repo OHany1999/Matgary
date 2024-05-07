828873426dae15995a4db43a1e631b9efb2bcdbc
OHany1999 <omar1999410@gmail.com>
2024-05-07 15:35:02 +0200

    7/5 update

--- lib/product_details/domain/repository/base_product_details_repository.dart
+++ lib/product_details/domain/repository/base_product_details_repository.dart
@@ -1,10 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:matgary/core/error/failure.dart';
+import 'package:matgary/product_details/domain/entities/add_favorite_entity.dart';
 import 'package:matgary/product_details/domain/entities/home_details_entity.dart';
+import 'package:matgary/product_details/domain/usecase/add_favorite_usecase.dart';
 import 'package:matgary/product_details/domain/usecase/get_product_details_usecase.dart';
 
 abstract class BaseProductDetailsRepository{
 
 Future<Either<Failure,ProductDetailsEntity>> getProductDetailsData(ProductDetailsParameters productDetailsParameters);
+Future<Either<Failure,AddFavoriteEntity>> addFavoriteData(AddFavoriteParameters addFavoriteParameters);
+
 
 }
---
+++ lib/product_details/domain/usecase/add_favorite_usecase.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:matgary/core/error/failure.dart';
+import 'package:matgary/core/usecase/base_usecase.dart';
+import 'package:matgary/product_details/domain/entities/add_favorite_entity.dart';
+import 'package:matgary/product_details/domain/repository/base_product_details_repository.dart';
+
+class AddFavoriteUseCase
+    extends BaseUseCase<AddFavoriteEntity, AddFavoriteParameters> {
+  final BaseProductDetailsRepository baseProductDetailsRepository;
+
+  AddFavoriteUseCase(this.baseProductDetailsRepository);
+
+  @override
+  Future<Either<Failure, AddFavoriteEntity>> call(
+      AddFavoriteParameters addFavoriteParameters) async {
+    return await baseProductDetailsRepository
+        .addFavoriteData(addFavoriteParameters);
+  }
+}
+
+class AddFavoriteParameters extends Equatable {
+  final int id;
+
+  const AddFavoriteParameters({required this.id});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [id];
+}
