78ae87a8e181e661855e07db0d29ebfd9fcada46
Omar <omar1999410@gmail.com>
2024-06-22 21:45:53 +0300

    22/6 update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:matgary/core/global/app_shared_pref.dart';
 import 'package:matgary/core/global/routes/routes.dart';
@@ -7,10 +8,13 @@ import 'package:matgary/core/services/services_locator.dart';
 
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await ServicesLocator().init();
-  final AppPreferences _appPref = sl<AppPreferences>();
-  print('tokeeeen = ${_appPref.getToken()}');
+  final AppPreferences appPref = sl<AppPreferences>();
+  print('token = ${appPref.getToken()}');
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
